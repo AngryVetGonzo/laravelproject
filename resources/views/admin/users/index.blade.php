7ba1dd222773163a87826a3f2a154fa1abba9222
jesus <chuyelregio5@yahoo.com>
2017-10-18 13:56:57 -0700

    displaying the users in admin/users page

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,34 @@
-<h1>hello world</h1>
+@extends('layouts.admin')
+
+@section('content')
+
+    <h1>users</h1>
+
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Created</th>
+            <th>Updated</th>
+          </tr>
+        </thead>
+        <tbody>
+        @if($users)
+            @foreach($users as $user)
+            <td>{{$user->id}}</td>
+            <td>{{$user->name}}</td>
+            <td>{{$user->email}}</td>
+            <td>{{$user->created_at->diffForHumans()}}</td>
+            <td>{{$user->updated_at->diffForHumans()}}</td>
+          </tr>
+            @endforeach
+
+        @endif
+
+        </tbody>
+      </table>
+    
+
+    @stop
